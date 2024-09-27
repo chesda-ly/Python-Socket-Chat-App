4e87e7f9c8fa487bfe45a9c3d230cc22fb1a230d
Sopheakchesda Ly <2023448ly@aupp.edu.kh>
2024-09-27 12:09:31 +0700

    Submission

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import threading
 
 PORT = 5050
 SERVER = "localhost"
@@ -7,35 +8,78 @@ ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-
+# Connect to the server
 def connect():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
     return client
 
-
-def send(client, msg):
-    message = msg.encode(FORMAT)
+# Send a message to the server
+def send(client, msg, receiver=None):
+    if receiver:
+        message = f"@{receiver} {msg}".encode(FORMAT)
+        
+    else:
+        message = msg.encode(FORMAT)
+        
     client.send(message)
 
+# Receive messages from the server
+def receive(client):
+    while True:
+        try:
+            msg = client.recv(1024).decode(FORMAT)
+            if msg:
+                print(msg)
+                
+            else:
+                break
+            
+        except:
+            client.close()
+            break
 
 def start():
     answer = input('Would you like to connect (yes/no)? ')
     if answer.lower() != 'yes':
         return
 
+    username = input('Enter your username: ')
     connection = connect()
+    
+    # Send username to server
+    send(connection, username)
+    
+    # Start a new thread to receive messages from the server
+    threading.Thread(target=receive, args=(connection,)).start()
+    
+    print("""Welcome to the Chat Room!
+- Messages will be broadcated to all clients INCLUDING ther server
+- Notices sent by the server will be displayed as [SERVER]: message
+- To send a private message, use @username message
+- To send a private message to multiople users, use @username1,username2 message
+- To disconnect, type 'q'
+Have Fun!
+----------------------------------------""")
     while True:
-        msg = input("Message (q for quit): ")
+        msg = input()
 
         if msg == 'q':
             break
-
-        send(connection, msg)
+        
+        if msg.startswith('@'):
+            try:
+                receiver, private_msg = msg[1:].split(' ', 1)
+                send(connection, private_msg, receiver)
+            except ValueError:
+                print('Invalid format. Please use @username message')
+        else:
+            send(connection, msg)
 
     send(connection, DISCONNECT_MESSAGE)
     time.sleep(1)
     print('Disconnected')
+    connection.close()
 
 
 start()
--- server.py
+++ server.py
@@ -10,44 +10,93 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-clients = set()
-clients_lock = threading.Lock()
+clients = []
+usernames = {}
 
+# Broadcast messages to all clients
+def broadcast(message, sender_client, receiver=None):
+    if receiver:
+        for client, username in usernames.items():
+            if username == receiver:
+                try:
+                    client.send(message)
+                    
+                except:
+                    client.close()
+                    
+                return
+    else:
+        for client in clients:
+            if client != sender_client:
+                try:
+                    client.send(message)
+                    
+                except:
+                    client.close()
+        
+# Announce messages to all clients             
+def announce():
+    while True:
+        msg = input()
+        if msg == "q":
+            print("Server is closed.")
+            server.close()
+            break
+        
+        else:
+            formatted_msg = f"[SERVER]: {msg}".encode(FORMAT)
+            broadcast(formatted_msg, None)
+                
+def handle_client(client):
+    username = client.recv(1024).decode(FORMAT)
+    usernames[client] = username
+    print(f"[NEW CONNECTION] {username} connected.")
+    broadcast(f"[SERVER]: {username} has joined the chat!".encode(FORMAT), client)    
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} Connected")
-
-    try:
-        connected = True
-        while connected:
-            msg = conn.recv(1024).decode(FORMAT)
-            if not msg:
+    while True:
+        try:
+            msg = client.recv(1024)
+            if msg == DISCONNECT_MESSAGE.encode(FORMAT):
+                print(f"[DISCONNECT] {usernames[client]} disconnected.")
+                broadcast(f"[SERVER]: {usernames[client]} has left the chat.".encode(FORMAT), client)
+                client.close()
+                clients.remove(client)
                 break
-
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-            with clients_lock:
-                for c in clients:
-                    c.sendall(f"[{addr}] {msg}".encode(FORMAT))
-
-    finally:
-        with clients_lock:
-            clients.remove(conn)
-
-        conn.close()
-
+            
+            elif msg.startswith(b"@"):
+                receivers, private_msg = msg[1:].decode(FORMAT).split(" ", 1)
+                recipients = receivers.split(",")
+                private_msg = f"{usernames[client]} [PRIVATE] - {private_msg}".encode(FORMAT)
+                for receiver in recipients:
+                    broadcast(private_msg, client, receiver.strip())
+                
+            elif msg:
+                msg = f"{usernames[client]}: {msg.decode(FORMAT)}".encode(FORMAT)
+                print(f"[MESSAGE RECEIVED] {msg.decode(FORMAT)}")
+                broadcast(msg, client)
+                
+            else:
+                break
+            
+        except:
+            clients.remove(client)
+            client.close()
+            break
 
 def start():
     print('[SERVER STARTED]!')
     server.listen()
+    threading.Thread(target=announce).start()
     while True:
-        conn, addr = server.accept()
-        with clients_lock:
-            clients.add(conn)
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
+        try:
+            client, addr = server.accept()
+            print(f"[NEW CONNECTION] {addr} connected.")
+            clients.append(client)
+            thread = threading.Thread(target=handle_client, args=(client,))
+            thread.start()
+            
+        except:
+            break
 
-
+print("[STARTING] Server is starting...")
 start()
